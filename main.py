46cc0505be0505e8635f2738caab7f1c28e421e7
Ich271 <76229784+Ich271@users.noreply.github.com>
2021-04-14 14:55:51 +0200

    Hangman release

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from _ast import keyword
 import random
 
-start = input(keyword("Start"))
 
 def generateRandomWord():
     with open("deutsch.txt") as wList:
@@ -11,13 +9,32 @@ def generateRandomWord():
         word = wLIstRead[ran]
         return word
 
-word = generateRandomWord()
-print(word)
 
-################################################
+case = {1: " ┌────┐\n o    │\n⁄│\   │\n⁄ \   ┴",
+        2: " ┌────┐\n o    │\n⁄│\   │\n⁄     ┴",
+        3: " ┌────┐\n o    │\n⁄│\   │\n      ┴",
+        4: " ┌────┐\n o    │\n⁄│    │\n      ┴",
+        5: " ┌────┐\n o    │\n │    │\n      ┴",
+        6: " ┌────┐\n o    │\n      │\n      ┴",
+        7: " ┌────┐\n      │\n      │\n      ┴",
+        8: "  ────┐\n      │\n      │\n      ┴",
+        9: "      ┐\n      │\n      │\n      ┴",
+        10: "      \n      │\n      │\n      ┴",
+        11: "      \n       \n      │\n      ┴",
+        12: "      \n       \n       \n      ┴",
+        13: "      \n       \n       \n       ",
+        }
+
+
+
+word = generateRandomWord()
+word = word.lower()
+
 letters = list(word)
 spaces = ["_"] * len(letters)
 print(spaces)
+
+versuche = 13
 
 while True:
     letter = input("ur guess! >>> ")
@@ -26,9 +43,14 @@ while True:
         for i in range(len(letters)):
             if letters[i] == letter:
                 spaces[i] = letter
-
-        print(spaces)
+    else:
+        versuche -= 1
+        print(case[versuche])
+        if versuche <= 1:
+            print("u died")
+            exit(0)
+    print(spaces)
 
     if "_" not in spaces:
         print("U won!")
-        break
+        exit(0)
